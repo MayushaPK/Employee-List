f8f85686c307a7f7ebf1e099c13c54d240090b99
MayushaPK <94427625+MayushaPK@users.noreply.github.com>
2021-11-27 04:50:13 +0530

    Add files via upload

--- application/models/Csv_import_model.php
+++ application/models/Csv_import_model.php
@@ -1,1 +1,30 @@
 
+<?php
+class Csv_import_model extends CI_Model 
+{    
+    function __construct() 
+    {
+        parent::__construct();       
+    }
+    
+    function get_emplist() 
+    {     
+        $query = $this->db->get('emp_list');
+        if ($query->num_rows() > 0) 
+        {
+            return $query->result_array();
+        } 
+        else 
+        {
+            return FALSE;
+        }
+    }
+    
+    function insert_csv($data)
+    {
+        $this->db->insert('emp_list', $data);
+    }
+}
+/*END OF FILE*/
+?>
+
